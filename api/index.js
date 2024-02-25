7ea746528c0e5fe0449f56b3fb07e0f6fae72399
Waniazlan <waniazlan27@gmail.com>
2024-02-25 23:46:37 +0800

    new commit

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 mongoose.connect(process.env.MONGO_URL)
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: 'https://hotpizzaria.netlify.app',
   methods: ["GET", "POST", "PUT", "DELETE"],
 
 }))
